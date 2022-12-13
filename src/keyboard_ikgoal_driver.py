be2f81d878eb1fececb0d89d2e871d9308295ecb
Alban Laflaquiere <alban.laflaquiere@sony.com>
2022-12-13 18:32:23 +0900

    formatting

--- src/keyboard_ikgoal_driver.py
+++ src/keyboard_ikgoal_driver.py
@@ -1,29 +1,30 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 
 import readchar
 import rospy
-from geometry_msgs.msg import PoseStamped, Vector3Stamped, QuaternionStamped, Pose
+from geometry_msgs.msg import Pose, PoseStamped, QuaternionStamped, Vector3Stamped
 from std_msgs.msg import Bool
-from relaxed_ik.msg import EEPoseGoals
+
 import RelaxedIK.Utils.transformations as T
+from relaxed_ik.msg import EEPoseGoals
 
-rospy.init_node('keyboard_ikgoal_driver')
+rospy.init_node("keyboard_ikgoal_driver")
 
-ik_goal_r_pub = rospy.Publisher('/ik_goal_r',PoseStamped,queue_size=5)
-ik_goal_l_pub = rospy.Publisher('/ik_goal_l',PoseStamped,queue_size=5)
-goal_pos_pub = rospy.Publisher('vive_position', Vector3Stamped)
-goal_quat_pub = rospy.Publisher('vive_quaternion', QuaternionStamped)
-ee_pose_goals_pub = rospy.Publisher('/relaxed_ik/ee_pose_goals', EEPoseGoals, queue_size=5)
-quit_pub = rospy.Publisher('/relaxed_ik/quit',Bool,queue_size=5)
+ik_goal_r_pub = rospy.Publisher("/ik_goal_r", PoseStamped, queue_size=5)
+ik_goal_l_pub = rospy.Publisher("/ik_goal_l", PoseStamped, queue_size=5)
+goal_pos_pub = rospy.Publisher("vive_position", Vector3Stamped)
+goal_quat_pub = rospy.Publisher("vive_quaternion", QuaternionStamped)
+ee_pose_goals_pub = rospy.Publisher("/relaxed_ik/ee_pose_goals", EEPoseGoals, queue_size=5)
+quit_pub = rospy.Publisher("/relaxed_ik/quit", Bool, queue_size=5)
 
 pos_stride = 0.015
 rot_stride = 0.055
 
-position_r = [0,0,0]
-rotation_r = [1,0,0,0]
+position_r = [0, 0, 0]
+rotation_r = [1, 0, 0, 0]
 
-position_l = [0,0,0]
-rotation_l = [1,0,0,0]
+position_l = [0, 0, 0]
+rotation_l = [1, 0, 0, 0]
 
 seq = 1
 rate = rospy.Rate(1000)
@@ -51,86 +52,85 @@ while not rospy.is_shutdown():
     ik_goal_l_pub.publish(pose)
 
     key = readchar.readkey()
-    if key == 'w':
+    if key == "w":
         position_r[0] += pos_stride
-    elif key == 'x':
+    elif key == "x":
         position_r[0] -= pos_stride
-    elif key == 'a':
+    elif key == "a":
         position_r[1] += pos_stride
-    elif key == 'd':
+    elif key == "d":
         position_r[1] -= pos_stride
-    elif key == 'q':
+    elif key == "q":
         position_r[2] += pos_stride
-    elif key == 'z':
+    elif key == "z":
         position_r[2] -= pos_stride
-    elif key == '1':
+    elif key == "1":
         euler = list(T.euler_from_quaternion(rotation_r))
         euler[0] += rot_stride
-        rotation_r = T.quaternion_from_euler(euler[0],euler[1],euler[2])
-    elif key == '2':
+        rotation_r = T.quaternion_from_euler(euler[0], euler[1], euler[2])
+    elif key == "2":
         euler = list(T.euler_from_quaternion(rotation_r))
         euler[0] -= rot_stride
-        rotation_r = T.quaternion_from_euler(euler[0],euler[1],euler[2])
-    elif key == '3':
+        rotation_r = T.quaternion_from_euler(euler[0], euler[1], euler[2])
+    elif key == "3":
         euler = list(T.euler_from_quaternion(rotation_r))
         euler[1] += rot_stride
-        rotation_r = T.quaternion_from_euler(euler[0],euler[1],euler[2])
-    elif key == '4':
+        rotation_r = T.quaternion_from_euler(euler[0], euler[1], euler[2])
+    elif key == "4":
         euler = list(T.euler_from_quaternion(rotation_r))
         euler[1] -= rot_stride
-        rotation_r = T.quaternion_from_euler(euler[0],euler[1],euler[2])
-    elif key == '5':
+        rotation_r = T.quaternion_from_euler(euler[0], euler[1], euler[2])
+    elif key == "5":
         euler = list(T.euler_from_quaternion(rotation_r))
         euler[2] += rot_stride
-        rotation_r = T.quaternion_from_euler(euler[0],euler[1],euler[2])
-    elif key == '6':
+        rotation_r = T.quaternion_from_euler(euler[0], euler[1], euler[2])
+    elif key == "6":
         euler = list(T.euler_from_quaternion(rotation_r))
         euler[2] -= rot_stride
-        rotation_r = T.quaternion_from_euler(euler[0],euler[1],euler[2])
+        rotation_r = T.quaternion_from_euler(euler[0], euler[1], euler[2])
 
-    elif key == 'i':
-        position_l[0] += pos_stride # left
-    elif key == 'm':
+    elif key == "i":
+        position_l[0] += pos_stride  # left
+    elif key == "m":
         position_l[0] -= pos_stride
-    elif key == 'j':
+    elif key == "j":
         position_l[1] += pos_stride
-    elif key == 'l':
+    elif key == "l":
         position_l[1] -= pos_stride
-    elif key == 'u':
+    elif key == "u":
         position_l[2] += pos_stride
-    elif key == 'n':
+    elif key == "n":
         position_l[2] -= pos_stride
-    elif key == '=':
+    elif key == "=":
         euler = list(T.euler_from_quaternion(rotation_l))
         euler[0] += rot_stride
-        rotation_l = T.quaternion_from_euler(euler[0],euler[1],euler[2])
-    elif key == '-':
+        rotation_l = T.quaternion_from_euler(euler[0], euler[1], euler[2])
+    elif key == "-":
         euler = list(T.euler_from_quaternion(rotation_l))
         euler[0] -= rot_stride
-        rotation_l = T.quaternion_from_euler(euler[0],euler[1],euler[2])
-    elif key == '0':
+        rotation_l = T.quaternion_from_euler(euler[0], euler[1], euler[2])
+    elif key == "0":
         euler = list(T.euler_from_quaternion(rotation_l))
         euler[1] += rot_stride
-        rotation_l = T.quaternion_from_euler(euler[0],euler[1],euler[2])
-    elif key == '9':
+        rotation_l = T.quaternion_from_euler(euler[0], euler[1], euler[2])
+    elif key == "9":
         euler = list(T.euler_from_quaternion(rotation_l))
         euler[1] -= rot_stride
-        rotation_l = T.quaternion_from_euler(euler[0],euler[1],euler[2])
-    elif key == '8':
+        rotation_l = T.quaternion_from_euler(euler[0], euler[1], euler[2])
+    elif key == "8":
         euler = list(T.euler_from_quaternion(rotation_l))
         euler[2] += rot_stride
-        rotation_l = T.quaternion_from_euler(euler[0],euler[1],euler[2])
-    elif key == '7':
+        rotation_l = T.quaternion_from_euler(euler[0], euler[1], euler[2])
+    elif key == "7":
         euler = list(T.euler_from_quaternion(rotation_l))
         euler[2] -= rot_stride
-        rotation_l = T.quaternion_from_euler(euler[0],euler[1],euler[2])
-    elif key == 'q':
+        rotation_l = T.quaternion_from_euler(euler[0], euler[1], euler[2])
+    elif key == "q":
         q = Bool()
         q.data = True
         quit_pub.publish(q)
-    elif key == 'c':
+    elif key == "c":
         rospy.signal_shutdown()
-
 
     pose = PoseStamped()
     pose.pose.position.x = position_r[0]
@@ -142,7 +142,6 @@ while not rospy.is_shutdown():
     pose.pose.orientation.y = rotation_r[2]
     pose.pose.orientation.z = rotation_r[3]
     ik_goal_r_pub.publish(pose)
-
 
     pose = PoseStamped()
     pose.pose.position.x = position_l[0]
